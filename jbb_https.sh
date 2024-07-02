39d9e6e1231d46569926a4cbb16a6e0f524b647c
abikesa <abikesa.sh@gmail.com>
2024-07-02 12:10:58 -0400

    new.workflow, with defaults

--- jbb_https.sh
+++ jbb_https.sh
@@ -1,21 +1,28 @@
-# mv app _build/html/app
-read -p "Enter your GitHub username: " GITHUB_USERNAME
-read -p "Enter your GitHub repository name: " REPO_NAME
-read -p "Enter your email address: " EMAIL_ADDRESS
-read -p "Enter your root directory (e.g., ~/Dropbox/1f.ἡἔρις,κ/1.ontology): " ROOT_DIR
-read -p "Enter the name of the subdirectory to be built within the root directory: " SUBDIR_NAME
+#! /bin/bash
+read -p "Enter your GitHub username (default: abikesa): " GITHUB_USERNAME
+GITHUB_USERNAME=${GITHUB_USERNAME:-abikesa}
+
+read -p "Enter your GitHub repository name (default: worthy): " REPO_NAME
+REPO_NAME=${REPO_NAME:-worthy}
+
+read -p "Enter your email address (default: abikesa.sh@gmail.com): " EMAIL_ADDRESS
+EMAIL_ADDRESS=${EMAIL_ADDRESS:-abikesa.sh@gmail.com}
+
+read -p "Enter your root directory (e.g., ~/Dropbox/1f.ἡἔρις,κ/1.ontology, default: /documents/rhythm): " ROOT_DIR
+ROOT_DIR=${ROOT_DIR:-/documents/rhythm}
+
+read -p "Enter the name of the subdirectory to be built within the root directory (default: local): " SUBDIR_NAME
+SUBDIR_NAME=${SUBDIR_NAME:-local}
+
 read -p "Enter your commit statement: " COMMIT_THIS
 
-# Build the book with Jupyter Book
 git config --local user.name "$GITHUB_USERNAME"
 git config --local user.email "$EMAIL_ADDRESS"
 
 cd "$(eval echo $ROOT_DIR)"
 
-rm -rf $SUBDIR_NAME/_build; cuts runtimes by 90%+;
 rm -rf $SUBDIR_NAME/_build
 jb build $SUBDIR_NAME
-mkdir -p $SUBDIR_NAME/_build/html/app && cp -r $SUBDIR_NAME/app/* $SUBDIR_NAME/_build/html/app
 rm -rf $REPO_NAME
 
 if [ -d "$REPO_NAME" ]; then
@@ -23,18 +30,15 @@ if [ -d "$REPO_NAME" ]; then
   exit 1
 fi
 
-# Cloning
 git clone "https://github.com/$GITHUB_USERNAME/$REPO_NAME.git"
 if [ ! -d "$REPO_NAME" ]; then
   echo "Failed to clone the repository. Check your GitHub username, repository name, and permissions."
   exit 1
 fi
 
-# Copy files from subdirectory to the current repository directory; restored $REPO_NAME!!!
 cp -r $SUBDIR_NAME/* $REPO_NAME
 cd $REPO_NAME
 
-# cd kitabo/website
 git add ./*
 git commit -m "$COMMIT_THIS"
 git remote -v
@@ -43,16 +47,13 @@ git remote set-url origin "https://github.com/$GITHUB_USERNAME/$REPO_NAME.git"
 git config --local user.name "$GITHUB_USERNAME"
 git config --local user.email "$EMAIL_ADDRESS"
 
-# Checkout the main branch``
+# Checkout the main branch
 git checkout main
 if [ $? -ne 0 ]; then
   echo "Failed to checkout the main branch. Make sure it exists in the repository."
   exit 1
 fi
 
-# Pushing changes
-# git config pull.rebase true
-# git pull
 git push -u origin main
 if [ $? -ne 0 ]; then
   echo "Failed to push to the repository. Check your credentials and GitHub permissions."
@@ -60,6 +61,4 @@ if [ $? -ne 0 ]; then
 fi
 
 ghp-import -n -p -f _build/html
-# cd ..
-# rm -rf $REPO_NAME
 echo "Jupyter Book content updated and pushed to $GITHUB_USERNAME/$REPO_NAME repository!"
