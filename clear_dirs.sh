a8d2398ae1160851697439dacd950f872e27747c
abikesa <abikesa.sh@gmail.com>
2024-08-07 18:30:46 -0400

    updated clear_dirs.sh, to clear main landing page dirs

--- clear_dirs.sh
+++ clear_dirs.sh
@@ -9,9 +9,15 @@ cd ~/documents/"$dir" || { echo "Directory not found"; exit 1; }
 
 # Loop through all items in the directory
 for item in *; do
-    # Check if the item is a directory and not "kitabo"
+    # Check if the item is a directory
     if [ -d "$item" ]; then
-        if [ "$item" != "kitabo" ] && ! [[ $item =~ ^[1-6]-.* ]]; then
+        # If the directory matches the pattern [1-6]-*
+        if [[ $item =~ ^[1-6]-.* ]]; then
+            # Remove the contents of the directory
+            rm -rf "$item"/*
+            echo "Deleted contents of directory $item"
+        # If the directory is not "kitabo" and does not match the pattern
+        elif [ "$item" != "kitabo" ]; then
             # Remove the directory and its contents
             rm -rf "$item"
             echo "Deleted directory $item"
